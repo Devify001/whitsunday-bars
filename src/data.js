e234bfb3995a1070ab88312d1fcb35a93c81cdfe
Tony Young <aryoung013@gmail.com>
2023-04-12 15:43:35 +1000

    first commit

---
+++ src/data.js
@@ -0,0 +1,17 @@
+let barData = [
+  {
+    id: "articleOne",
+    name: "Name One",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+    summ: "Point One",
+    img: "images/img-1.jpg",
+  },
+
+  {
+    id: "articleTwo",
+    name: "Name Two",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+    summ: "Point One",
+    img: "images/img-2.jpg",
+  },
+];
